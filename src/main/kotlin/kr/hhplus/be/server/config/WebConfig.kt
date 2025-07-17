70dbbdf857b3b5add33378d2dee5867f565df89e
박서희 <joypark1@douzone.com>
2025-07-18 07:36:57 +0900

    :memo: mock api 호출을 위한 allow 추가

---
+++ src/main/kotlin/kr/hhplus/be/server/config/WebConfig.kt
@@ -0,0 +1,20 @@
+package kr.hhplus.be.server.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebConfig : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins(
+                "http://localhost:8080",
+                "https://joyseohee.github.io/hhplus-concert-server"
+            )
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            .allowedHeaders("*")
+            .allowCredentials(true)
+            .maxAge(3600)
+    }
+}
