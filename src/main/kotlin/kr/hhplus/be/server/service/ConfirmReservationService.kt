963f64fc76c74b03744d856e102ee62b41aebf9e
joy <sh940311@gmail.com>
2025-07-25 08:37:58 +0900

    :sparkles: 좌석점유 만료 확인 로직 추가

--- src/main/kotlin/kr/hhplus/be/server/service/ConfirmReservationService.kt
+++ src/main/kotlin/kr/hhplus/be/server/service/ConfirmReservationService.kt
@@ -25,17 +25,20 @@ class ConfirmReservationService(
         val seatHold = seatHoldRepository.findBySeatId(input.seatId)
             ?: throw IllegalArgumentException("유효하지 않은 좌석 예약입니다.")
 
-        // 점유된 좌석의 유효성 검사
-        seatHold.isValid(userId)
+        // 좌석 점유 상태 갱신
+        val checkedSeatHold = seatHold.expireIfNeeded()
+        if (checkedSeatHold.status == SeatHold.Status.EXPIRED && checkedSeatHold.status != SeatHold.Status.EXPIRED) {
+            seatHoldRepository.save(checkedSeatHold)
+        }
+        checkedSeatHold.isValid(userId)
 
         // 좌석 정보 확인
-        val seat = seatRepository.findById(seatHold.seatId)
+        val seat = seatRepository.findById(checkedSeatHold.seatId)
             ?: throw IllegalArgumentException("존재하지 않는 좌석입니다.")
 
-        // 결제
-        val userBalance = userBalanceRepository.findById(seatHold.userId)
+        // 사용자 검증 및 결제
+        val userBalance = userBalanceRepository.findById(checkedSeatHold.userId)
             ?: throw IllegalArgumentException("사용자가 존재하지 않습니다.")
-
         userBalanceRepository.save(
             userBalance.use(seat.price)
         )
@@ -44,14 +47,15 @@ class ConfirmReservationService(
         val reservation = reservationRepository.save(
             Reservation.create(
                 reservationUuid = input.reservationUuid,
-                userId = seatHold.userId,
-                concertId = seatHold.concertId,
-                seatId = seatHold.seatId,
+                userId = checkedSeatHold.userId,
+                concertId = checkedSeatHold.concertId,
+                seatId = checkedSeatHold.seatId,
                 reservedAt = Instant.now(),
                 price = seat.price
             )
         )
 
+        // 점유상태 업데이트
         seatHoldRepository.save(
             SeatHold.create(
                 seatHoldId = seatHold.seatHoldId,
