8f1d3e9577cab0c9ef7e880b54790f1e2e99a222
joy <sh940311@gmail.com>
2025-07-25 07:40:55 +0900

    :sparkles: 잔고 Controller 추가

---
+++ src/main/kotlin/kr/hhplus/be/server/controller/BalanceController.kt
@@ -0,0 +1,11 @@
+package kr.hhplus.be.server.controller
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/v1/balance")
+class BalanceController(
+) {
+
+}
