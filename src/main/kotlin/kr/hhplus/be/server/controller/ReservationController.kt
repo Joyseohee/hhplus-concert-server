86b1658e8d117f5f0a69b6752a354201565abad0
joy <sh940311@gmail.com>
2025-07-25 08:06:08 +0900

    :art: ReservationController.kt 리팩토링

--- src/main/kotlin/kr/hhplus/be/server/controller/ReservationController.kt
+++ src/main/kotlin/kr/hhplus/be/server/controller/ReservationController.kt
@@ -19,83 +19,83 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/api/v1/reservations")
 class ReservationController(
-    val listConcertService: ListConcertService,
-    val listSeatService: ListSeatService,
-    val holdSeatService: HoldSeatService,
-    val confirmReservationService: ConfirmReservationService
+	val listConcertService: ListConcertService,
+	val listSeatService: ListSeatService,
+	val holdSeatService: HoldSeatService,
+	val confirmReservationService: ConfirmReservationService
 ) : SwaggerReservationController {
 
-    @GetMapping("/concerts")
-    override fun getConcerts(
-        //		token: String
-    ): ResponseEntity<ApiResponse<ListConcertService.Output>> {
+	@GetMapping("/concerts")
+	override fun getConcerts(
+		//		token: String
+	): ResponseEntity<ApiResponse<ListConcertService.Output>> {
 
-        val concerts = listConcertService.listConcerts()
+		val concerts = listConcertService.listConcerts()
 
-        return ResponseEntity.ok(
-            ApiResponse(
-                code = "SUCCESS",
-                message = "조회 성공",
-                data = concerts
-            )
-        )
-    }
+		return ResponseEntity.ok(
+			ApiResponse(
+				code = "SUCCESS",
+				message = "조회 성공",
+				data = concerts
+			)
+		)
+	}
 
-    @GetMapping("/concerts/{concertId}/seats")
-    override fun getSeats(
-        //        token: String,
-        @RequestHeader(value = "X-Client-Id", required = true) userId: Long,
-        @PathVariable(required = true) concertId: Long
-    ): ResponseEntity<ApiResponse<ListSeatService.Output>> {
+	@GetMapping("/concerts/{concertId}/seats")
+	override fun getSeats(
+		//        token: String,
+		@RequestHeader(value = "X-Client-Id", required = true) userId: Long,
+		@PathVariable(required = true) concertId: Long
+	): ResponseEntity<ApiResponse<ListSeatService.Output>> {
 
-        val seats = listSeatService.listAvailableSeats(concertId, userId)
+		val seats = listSeatService.listAvailableSeats(concertId, userId)
 
-        return ResponseEntity.ok(
-            ApiResponse(
-                code = "SUCCESS",
-                message = "좌석 조회 성공",
-                data = seats
-            )
-        )
-    }
+		return ResponseEntity.ok(
+			ApiResponse(
+				code = "SUCCESS",
+				message = "좌석 조회 성공",
+				data = seats
+			)
+		)
+	}
 
-    @PostMapping("/concerts/{concertId}/seats/hold")
-    override fun holdSeats(
-        //        token: String,
-        @RequestHeader(value = "X-Client-Id", required = true) userId: Long,
-        @RequestBody seatsHoldRequest: HoldSeatService.Input
-    ): ResponseEntity<ApiResponse<HoldSeatService.Output>> {
+	@PostMapping("/concerts/{concertId}/seats/hold")
+	override fun holdSeats(
+		//        token: String,
+		@RequestHeader(value = "X-Client-Id", required = true) userId: Long,
+		@RequestBody seatsHoldRequest: HoldSeatService.Input
+	): ResponseEntity<ApiResponse<HoldSeatService.Output>> {
 
-        val holdSeat = holdSeatService.holdSeat(
-            userId = userId,
-            input = seatsHoldRequest,
-        )
+		val holdSeat = holdSeatService.holdSeat(
+			userId = userId,
+			input = seatsHoldRequest,
+		)
 
-        return ResponseEntity.ok(
-            ApiResponse(
-                code = "SUCCESS",
-                message = "좌석 점유 성공",
-                data = holdSeat
-            )
-        )
-    }
+		return ResponseEntity.ok(
+			ApiResponse(
+				code = "SUCCESS",
+				message = "좌석 점유 성공",
+				data = holdSeat
+			)
+		)
+	}
 
-    @PostMapping("/")
-    override fun confirmedReservation(
-        //        token: String,
-        @RequestHeader(value = "X-Client-Id", required = true) userId: Long,
-        @RequestBody reservationRequest: ConfirmReservationService.Input
-    ): ResponseEntity<ApiResponse<ConfirmReservationService.Output>> {
-        val reservation = confirmReservationService.confirmReservation(
-            userId = userId,
-            input = reservationRequest
-        )
-        return ResponseEntity.ok(
-            ApiResponse(
-                code = "SUCCESS",
-                message = "예약 확정 성공",
-                data = reservation
-            )
-        )
-    }
+	@PostMapping("/")
+	override fun confirmedReservation(
+		//        token: String,
+		@RequestHeader(value = "X-Client-Id", required = true) userId: Long,
+		@RequestBody reservationRequest: ConfirmReservationService.Input
+	): ResponseEntity<ApiResponse<ConfirmReservationService.Output>> {
+		val reservation = confirmReservationService.confirmReservation(
+			userId = userId,
+			input = reservationRequest
+		)
+		return ResponseEntity.ok(
+			ApiResponse(
+				code = "SUCCESS",
+				message = "예약 확정 성공",
+				data = reservation
+			)
+		)
+	}
 }
