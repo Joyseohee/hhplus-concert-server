d3544d47beb67c9de455b99b336f476030bd9a22
joy <sh940311@gmail.com>
2025-07-25 07:41:56 +0900

    :sparkles: 잔고 조회

--- src/main/kotlin/kr/hhplus/be/server/controller/BalanceController.kt
+++ src/main/kotlin/kr/hhplus/be/server/controller/BalanceController.kt
@@ -1,11 +1,35 @@
 package kr.hhplus.be.server.controller
 
+import kr.hhplus.be.server.controller.swagger.SwaggerBalanceController
+import kr.hhplus.be.server.service.ChargeBalanceService
+import kr.hhplus.be.server.service.GetBalanceService
+import kr.hhplus.be.server.support.ApiResponse
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.util.*
 
 @RestController
 @RequestMapping("/api/v1/balance")
-class BalanceController(
+class BalanceController2(
+	private val getBalanceService: GetBalanceService,
 ) {
-
+	@GetMapping("/")
+	fun getBalance(
+		@RequestHeader(value = "X-Client-Id", required = true) userId: Long,
+	): ResponseEntity<ApiResponse<GetBalanceService.Output>>{
+		val balance = getBalanceService.getBalance(GetBalanceService.Input(userId))
+		// 비즈니스 로직 구현
+		return ResponseEntity.ok(
+			ApiResponse(
+				code = "SUCCESS",
+				message = "잔액 조회 성공",
+				data = balance
+			)
+		)
+	}
 }
