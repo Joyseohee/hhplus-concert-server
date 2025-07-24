92e00fd1626461eae58837fb51c58db5124cecf7
joy <sh940311@gmail.com>
2025-07-25 07:12:46 +0900

    :wrench: 설정파일 수정
    - build.gradle.kts에 테스트 의존성 추가
    - docker-compose.yml 및 도커 설정 off

--- build.gradle.kts
+++ build.gradle.kts
@@ -58,6 +58,10 @@ dependencies {
 	testImplementation("org.testcontainers:junit-jupiter")
 	testImplementation("org.testcontainers:mysql")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	testImplementation("io.mockk:mockk:1.13.10")
+	testImplementation("io.kotest:kotest-runner-junit5:5.8.1")
+	testImplementation("io.kotest:kotest-assertions-core:5.8.1")
+	testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.3")
 
 	// 	Docs
 	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.5.0")
@@ -66,4 +70,6 @@ dependencies {
 tasks.withType<Test> {
 	useJUnitPlatform()
 	systemProperty("user.timezone", "UTC")
+	systemProperty("spring.profiles.active", "test")
 }
+
