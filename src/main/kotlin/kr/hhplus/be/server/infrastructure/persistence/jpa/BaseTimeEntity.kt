cdcfbcd87d0fa2d72db0beda56137860eae53ee8
joy <sh940311@gmail.com>
2025-07-31 00:24:24 +0900

    :art: jpa BaseTimeEntity.kt 추가

---
+++ src/main/kotlin/kr/hhplus/be/server/infrastructure/persistence/jpa/BaseTimeEntity.kt
@@ -0,0 +1,18 @@
+package kr.hhplus.be.server.infrastructure.persistence.jpa
+
+import jakarta.persistence.Column
+import jakarta.persistence.MappedSuperclass
+import org.hibernate.annotations.CreationTimestamp
+import org.hibernate.annotations.UpdateTimestamp
+import java.time.Instant
+
+@MappedSuperclass
+abstract class BaseTimeEntity {
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    open val createdAt: Instant = Instant.now()
+
+    @UpdateTimestamp
+    @Column(name = "modified_at")
+    open val modifiedAt: Instant? = Instant.now()
+}
