db961fd966094bb7931175b62258c70f509ae482
박서희 <joypark1@douzone.com>
2025-07-30 12:29:53 +0900

    :art: 예약 객체 정책 추가

--- src/main/kotlin/kr/hhplus/be/server/domain/Reservation.kt
+++ src/main/kotlin/kr/hhplus/be/server/domain/Reservation.kt
@@ -31,5 +31,33 @@ data class Reservation private constructor(
 				price = price
 			)
 		}
+
+		fun reserve(
+			reservationId: Long? = null,
+			reservationUuid: String,
+			userId: Long,
+			concertId: Long,
+			seatId: Long,
+			reservedAt: Instant,
+			price: Long
+		): Reservation {
+			if (reservationUuid.isBlank()) {
+				throw IllegalArgumentException("Reservation UUID must not be blank")
+			}
+
+			if (price <= 0) {
+				throw IllegalArgumentException("User ID must be greater than zero")
+			}
+
+			return create(
+				reservationId = reservationId,
+				reservationUuid = reservationUuid,
+				userId = userId,
+				concertId = concertId,
+				seatId = seatId,
+				reservedAt = reservedAt,
+				price = price
+			)
+		}
 	}
 }
