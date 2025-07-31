3c04fbc5b8fc324f1d0a9a3309b2e9fea60f441e
joy <sh940311@gmail.com>
2025-08-01 07:40:07 +0900

    :triangular_flag_on_post: 스케줄러 적용 보류

--- src/main/kotlin/kr/hhplus/be/server/ConcertApplication.kt
+++ src/main/kotlin/kr/hhplus/be/server/ConcertApplication.kt
@@ -1,10 +1,14 @@
 package kr.hhplus.be.server
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Profile
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 import org.springframework.scheduling.annotation.EnableScheduling
 
-@EnableScheduling
+// @EnableScheduling
 @SpringBootApplication
 class ConcertApplication
 
