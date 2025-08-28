9d33579d9fd57f15c1b5fd4749cccf7997020a63
joy <sh940311@gmail.com>
2025-08-29 07:14:03 +0900

    :white_check_mark: 기존 테스트에서 불필요한 내용 제거 및 수정

--- src/test/kotlin/kr/hhplus/be/server/concurrency/UserBalanceConcurrencyTest.kt
+++ src/test/kotlin/kr/hhplus/be/server/concurrency/UserBalanceConcurrencyTest.kt
@@ -71,90 +71,10 @@ class UserBalanceConcurrencyTest @Autowired constructor(
         }
     }
 
-    given("동시성 테스트 - 잔액 차감") {
-        `when`("여러 스레드가 동시에 잔액을 차감할 때") {
-            then("토큰이 만료되므로 한 개의 요청만 처리된다") {
-                val user = userBalanceRepository.save(
-                    UserBalance.create(
-                        balance = initialBalance
-                    )
-                )
-
-                val seats = listOf(
-                    Seat.create(seatNumber = 1, price = amount),
-                    Seat.create(seatNumber = 2, price = amount)
-                ).map { seatRepository.save(it) }
-
-                queueTokenRepository.save(
-                    QueueToken.create(
-                        userId = user.userId!!
-                    )
-                )
-                expireStatusScheduler.expireStatuses()
-                val threadCount = 2
-                val latch = CountDownLatch(threadCount)
-                val executor = Executors.newFixedThreadPool(threadCount)
-
-                executor.submit {
-                    try {
-                        val hold = seatHoldRepository.save(
-                            // 좌석 점유 요청을 생성합니다.
-                            SeatHold.create(
-                                seatHoldUuid = UUID.randomUUID().toString(),
-                                userId = user.userId,
-                                concertId = 1L, // 임의의 콘서트 ID
-                                seatId = seats.get(0).seatId!!,
-                            )
-                        )
-
-                        reservationUseCase.confirmReservation(
-                            userId = user.userId,
-                            input = ConfirmReservationUseCase.Input(
-                                reservationUuid = UUID.randomUUID().toString(),
-                                seatHoldUuid = hold.seatHoldUuid
-                            )
-                        )
-                    } finally {
-                        latch.countDown()
-                    }
-                }
-                executor.submit {
-                    try {
-                        val hold = seatHoldRepository.save(
-                            // 좌석 점유 요청을 생성합니다.
-                            SeatHold.create(
-                                seatHoldUuid = UUID.randomUUID().toString(),
-                                userId = user.userId,
-                                concertId = 1L, // 임의의 콘서트 ID
-                                seatId = seats.get(1).seatId!!,
-                            )
-                        )
-
-                        reservationUseCase.confirmReservation(
-                            userId = user.userId,
-                            input = ConfirmReservationUseCase.Input(
-                                reservationUuid = UUID.randomUUID().toString(),
-                                seatHoldUuid = hold.seatHoldUuid
-                            )
-                        )
-                    } finally {
-                        latch.countDown()
-                    }
-                }
-                latch.await()
-
-                val finalBalance = userBalanceRepository.findById(user.userId)?.balance ?: 0
-                println("최종 잔액: $finalBalance")
-
-                finalBalance shouldBe initialBalance - amount // 100_000 - 1_000
-            }
-        }
-    }
-
     // 충전과 차감을 동시에 진행하는 테스트로, 충전과 차감이 동시에 이루어질 때 최종 잔액이 올바르게 계산되는지 확인하는 테스트입니다.
     given("동시성 테스트 - 잔액 충전 및 차감") {
         `when`("여러 스레드가 동시에 잔액을 충전과 차감을 요청할 때") {
-            then("충전은 모두 성공해야하고 사용은 한 번만 성공해야 한다") {
+            then("충전과 차감이 각각 동일한 횟수로 이루어졌다면 최종 잔액이 초기 잔액과 동일해야 한다") {
                 val user = userBalanceRepository.save(
                     UserBalance.create(
                         balance = initialBalance
@@ -219,7 +139,7 @@ class UserBalanceConcurrencyTest @Autowired constructor(
 
                 val finalBalance = userBalanceRepository.findById(user.userId)?.balance ?: 0
 
-                finalBalance shouldBe initialBalance + amount // 충전은 두 번 모두 정상적으로 이뤄지지만 결제는 좌석 당 한 번만 가능하다.
+                finalBalance shouldBe initialBalance
             }
         }
     }
