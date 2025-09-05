52a209c4a44a26a9a169d790bd54f15d2d9eccde
joy <sh940311@gmail.com>
2025-09-05 09:37:19 +0900

    :sparkles: 카프카 config 추가

---
+++ src/main/kotlin/kr/hhplus/be/server/infrastructure/message/kafka/config/KafkaConfig.kt
@@ -0,0 +1,10 @@
+package kr.hhplus.be.server.infrastructure.message.kafka.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.kafka.annotation.EnableKafka
+
+@EnableKafka
+@Configuration
+class KafkaConfig {
+
+}
