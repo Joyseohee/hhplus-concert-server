9cc87acc8d5492a5fe59dcf7a4b5baf71ec62cf2
joy <sh940311@gmail.com>
2025-09-05 09:43:47 +0900

    :test_tube: 컨테이너에 카프카 설정 추가

--- src/test/kotlin/kr/hhplus/be/server/KotestIntegrationSpec.kt
+++ src/test/kotlin/kr/hhplus/be/server/KotestIntegrationSpec.kt
@@ -2,20 +2,18 @@ package kr.hhplus.be.server
 
 import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.extensions.spring.SpringExtension
-import kr.hhplus.be.server.config.SharedKafka
-import org.junit.jupiter.api.BeforeAll
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.DynamicPropertyRegistry
 import org.springframework.test.context.DynamicPropertySource
 import org.testcontainers.containers.GenericContainer
 import org.testcontainers.containers.MySQLContainer
-import org.testcontainers.junit.jupiter.Testcontainers
+import org.testcontainers.junit.jupiter.Container
+import org.testcontainers.kafka.KafkaContainer
 import org.testcontainers.utility.DockerImageName
 
 @SpringBootTest
 @ActiveProfiles("test")
-@Testcontainers
 abstract class KotestIntegrationSpec(
     spec: BehaviorSpec.() -> Unit
 ) : BehaviorSpec(spec) {
@@ -33,21 +31,23 @@ abstract class KotestIntegrationSpec(
             start()
         }
 
+        @Container
         @JvmStatic
-        @BeforeAll
-        fun startKafka() {
-            SharedKafka.kafka // lazy start
-        }
+        val image = DockerImageName.parse("apache/kafka:3.8.0") // 예시: 3.6.x~3.8.x 아무거나 안정된 것
+        val kafka = KafkaContainer(image)
+            .apply {
+                start()  // Kafka 컨테이너 시작
+            }
 
         @JvmStatic
         @DynamicPropertySource
-        fun config(registry: DynamicPropertyRegistry) {
+        fun overrideProps(registry: DynamicPropertyRegistry) {
             registry.add("spring.datasource.url") { mysql.jdbcUrl }
             registry.add("spring.datasource.username") { mysql.username }
             registry.add("spring.datasource.password") { mysql.password }
             registry.add("spring.data.redis.host") { redis.host }
             registry.add("spring.data.redis.port") { redis.getMappedPort(6379) }
-            registry.add("spring.kafka.bootstrap-servers") { SharedKafka.kafka.bootstrapServers }
+            registry.add("spring.kafka.bootstrap-servers") { kafka.bootstrapServers }
         }
     }
 
