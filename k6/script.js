2ce5ca6700e04908e3513cd4d1d7addc59ab6459
joy <sh940311@gmail.com>
2025-09-12 07:49:41 +0900

    ** k6

--- k6/script.js
+++ k6/script.js
@@ -1,6 +1,5 @@
 import http from 'k6/http';
-import {check, sleep} from 'k6';
-import {uuidv4} from 'https://jslib.k6.io/k6-utils/1.4.0/index.js';
+import {check} from 'k6';
 
 const BASE = __ENV.BASE_URL || 'http://localhost:8080';
 const CONCERT_ID = 1;
@@ -8,9 +7,9 @@ const SEAT_ID = 1; // 경합 좌석
 
 export const options = {
     setupTimeout: '180s',   // 3분까지 허용
-    // 여러 시나리오 동시 구동
+
     scenarios: {
-        // // B: 대기열 토큰 발급 + 순번 조회
+        // A : 대기열 토큰 발급 + 순번 조회
         queue_admission: {
             executor: 'ramping-vus',
             startVUs: 0,
@@ -24,21 +23,9 @@ export const options = {
             tags: { api: 'queue' },
 
         },
-        // ramp: {
-        //     executor: 'ramping-arrival-rate',
-        //     startRate: 50, timeUnit: '1s',
-        //     preAllocatedVUs: 200, maxVUs: 1000,
-        //     stages: [
-        //         { target: 200, duration: '2m' },
-        //         { target: 400, duration: '2m' },
-        //         { target: 600, duration: '2m' },
-        //     ],
-        //     exec: 'queueFlow',
-        //     tags: { api: 'queue' },
-        // }
     },
     thresholds: {
-        'http_req_duration{api:queue}': [ 'p(90)<300', 'p(95)<500', 'max<2000' ],
+        'http_req_duration{api:queue}': [ 'p(90)<1000', 'p(95)<1000', 'max<2000' ],
         'http_req_failed{api:queue}':  [ 'rate<0.01' ],
     },
     summaryTrendStats: ['avg','min','med','p(75)','p(90)','p(95)','p(99)','max'],
@@ -53,12 +40,8 @@ function hdrUser(userId) {
     return { headers: { 'Content-Type': 'application/json', 'User-Id': String(userId) } };
 }
 
-function hdrToken(token) {
-    return { headers: { 'Content-Type': 'application/json', 'Queue-Token': String(token) } };
-}
-
 export function setup() {
-    // 예: 테스트에 필요한 사용자 1000명과 콘서트 좌석 생성
+    // 테스트에 필요한 사용자 1000명과 콘서트 좌석 생성
     for (let i = 1; i <= 1000; i++) {
         http.post('http://localhost:8080/api/v1/users', JSON.stringify({
             balance: 200000,
@@ -76,78 +59,10 @@ export function setup() {
     return {ready: true}; // 이후 exec 함수들에서 접근 가능
 }
 
-// 시나리오 B: 대기열 토큰 발급 + 순번 조회
+// 시나리오 A: 대기열 토큰 발급 + 순번 조회
 export function queueFlow() {
     const userId = Math.floor(Math.random() * 1000) + 1;
     // 토큰 발급
     const issue = http.post(`${BASE}/api/v1/queue/token`, null, hdrUser(userId));
     check(issue, { 'queue issue ok': (r) => [200, 201].includes(r.status) });
-}
-
-// 시나리오 C: 잔액 충전/예약 확정 혼합 (동일 userId 집중)
-export function balanceRace() {
-    const userId = 42; // 동일 유저에 경합 유발
-    // 충전
-    const chargeBody = JSON.stringify({ amount: 1000 });
-    const charge = http.post(`${BASE}/api/v1/balance/charge`, chargeBody, hdrUser(userId));
-    check(charge, { 'charge ok': (r) => [200, 409, 400].includes(r.status) });
-
-    // 토큰 발급
-    const issue = http.post(`${BASE}/api/v1/queue/tokens`, null, hdrUser(userId));
-
-    // 토큰 확인 (간헐)
-    if (Math.random() < 0.5) {
-        const token = json(issue).data?.token || json(issue).token;
-        if (token) {
-            // 선행 점유
-            const seatHoldUuid = uuidv4()
-            const body = JSON.stringify({ seatHoldUuid: seatHoldUuid, concertId: CONCERT_ID, seatId: SEAT_ID });
-            const hold = http.post(`${BASE}/api/v1/reservations/concerts/${CONCERT_ID}/seats/hold`, body, hdrToken(token));
-
-            // 예약 확정
-            const seatId = SEAT_ID;
-            const confirmBody = JSON.stringify({ reservationUuid: uuidv4(), seatId, seatHoldUuid: seatHoldUuid });
-            const confirm = http.post(`${BASE}/api/v1/reservations`, confirmBody, hdrToken(token));
-            check(confirm, { 'confirm ok': (r) => [200, 400, 409].includes(r.status) });
-
-            sleep(0.2);
-        }
-    }
-}
-
-// 시나리오 A: 동일 좌석 점유 경합
-export function seatHoldRace() {
-    // 토큰 발급
-    const issue = http.post(`${BASE}/api/v1/queue/tokens`, null, hdrUser(userId));
-
-    // 토큰 확인 (간헐)
-    if (Math.random() < 0.5) {
-        const token = json(issue).data?.token || json(issue).token;
-        if (token) {
-            const body = JSON.stringify({ seatHoldUuid: uuidv4(), concertId: CONCERT_ID, seatId: SEAT_ID });
-            const res = http.post(`${BASE}/api/v1/reservations/concerts/${CONCERT_ID}/seats/hold`, body, hdrToken(token));
-
-            check(res, {
-                'hold: 200 or 409': (r) => [200, 409, 400].includes(r.status),
-            });
-        }
-    }
-
-}
-
-
-// 시나리오 D: 좌석 목록 조회
-export function listSeats() {
-    // 토큰 발급
-    const issue = http.post(`${BASE}/api/v1/queue/tokens`, null, hdrUser(userId));
-
-    // 토큰 확인 (간헐)
-    if (Math.random() < 0.5) {
-        const token = json(issue).data?.token || json(issue).token;
-        if (token) {
-            const res = http.get(`${BASE}/api/v1/reservations/concerts/${CONCERT_ID}/seats`, hdrToken(token));
-            check(res, { 'list ok': (r) => r.status === 200 });
-        }
-    }
-
 }
